8a854fd2220f607673eff9e2b37d0ce14e6fc4c4
jhuna <smruti.bpd@gmail.com>
2016-11-11 22:59:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,17 @@
 console.log('Loaded!');
 
 //rewrite text
-var element = document.getElementById("ajit");
-element.innerHTML = 'new value';
+//var element = document.getElementById("ajit");
+//element.innerHTML = 'new value';
 
 //move the image
 var img = document.getElementById("madison");
 img.onclick = function () { 
-    
-    img.style.marginLeft = '100px';
+    function moveRight() {
+        marginLeft = 0;
+        marginLeft = marginLeft + 10;
+        img.style.marginLeft = marginLeft + 'px';
+    }
+    var interval = setInterval(moveRight, 100);
+//img.style.marginLeft = '100px';
 };
