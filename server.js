6247ac616306e7fb7bb85cca9bd78fe3d810a2a3
jhuna <smruti.bpd@gmail.com>
2016-11-11 14:46:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,12 @@ database management and security.
 <h4>
 PROJECTS
 </h4>
-
+<ul>
+<li>Design and installation of Solar power plant (September 2013 - March 2014)</li>
+<li>B.Tech project completed at BCET, Sergarh/Bhubaneshwar.</li>
+<li>Project Sponsored by Innodust, Bhubaneswar</li>
+<li>Project Guide: Lect. Chakradhar Panda</li>
+</ul>
 <h4>
 ACADEMIC INTERESTS
 </h4>
