e12d2a160ac2185fccf7bd28c7175ced5ad00f12
jhuna <smruti.bpd@gmail.com>
2016-11-11 22:10:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,10 @@
 console.log('Loaded!');
 
 //rewrite text
-var element = document.getElementById('ajit');
-
+var element = document.getElementById("ajit");
 element.innerHTML = 'new value';
 
 //move the image
-var img = document.getElementById("madi");
-img.onclick  = function() {
-    img.style.marginleft = "100px";
+var img = document.getElementById("madison");
+img.onclick = function() { img.style.marginleft = "100px";
 };
