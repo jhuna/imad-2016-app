7da9d7787e89fba09f38f97fa3890d689d12723d
jhuna <smruti.bpd@gmail.com>
2016-11-12 19:26:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,19 @@ var img = document.getElementById("madison");
 //img.style.marginLeft = '100px';
 };
 
+var button= document.getElementById('counter');
+button.onclick = function () {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest()) {
+        if (request.status == 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }    
+        }
+    };
+
+    request.open('GET', 'http://jhuna.imad.hasura-app.io/counter', 'true' );
+    request.send(null);
+};
