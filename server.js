e0fafb6cf0942941e2b4f1109185536914e9dc3e
jhuna <smruti.bpd@gmail.com>
2016-11-12 23:11:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -226,7 +226,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names = [];
-app.get('submit-name/name', function(req,res) {
+app.get('/submit-name/:name', function(req,res) {
   var name = req.params.name;
   names.push(name);
   res.send(JSON.stringify(names));
